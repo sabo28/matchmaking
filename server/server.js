9c0e3fc60f108c24c9b7bf5e8fd5f13b16b2606a
sabo28 <labeeb.malik318@gmail.com>
2023-01-06 12:14:06 +0100

    Initialize project

---
+++ server/server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Verbinde dich mit der Datenbank
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Model für Lobby-Dokumente
+const Lobby = mongoose.model('Lobby', new mongoose.Schema({
+  user1: {
+    type: String,
+    required: true
+  },
+  user2: {
+    type: String,
+    required: false
+  }
+}));
+
+// Websocket-Server
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT"]
+  }
+});
+
+// Websocket-Verbindungen speichern
+const connections = new Map();
+
+io.on('connection', (socket) => {
+  // Verbindungs-ID generieren und speichern
+  const id = Math.random().toString(36).substr(2);
+  connections.set(id, socket);
+
+  // Verbindung trennen
+  socket.on('disconnect', () => {
+    connections.delete(id);
+  });
+});
+
+// Benachrichtigungen senden
+const sendNotification = (lobby) => {
+  if (lobby.user2) {
+    // Benachrichtigung an beide Benutzer senden
+    connections.forEach((socket) => {
+      socket.emit('lobby-update', lobby);
+    });
+  } else {
+    // Benachrichtigung nur an Benutzer 1 senden
+    connections.forEach((socket) => {
+      if (socket.user === lobby.user1) {
+        socket.emit('lobby-update', lobby);
+      }
+    });
+  }
+};
+
+// Parsing middleware
+app.use(express.json());
+app.use(cors());
+
+// Route zum Erstellen einer Lobby
+app.post('/lobbies', async (req, res) => {
+  const { user } = req.body;
+  const lobby = new Lobby({ user1: user });
+  await lobby.save();
+  sendNotification(lobby);
+  res.send(lobby._id);
+});
+
+// Route zum Suchen verfügbarer Lobbies
+app.get('/lobbies', async (req, res) => {
+  const lobbies = await Lobby.find({ user2: { $exists: false } });
+  res.send(lobbies);
+});
+
+// Route zum Beitreten einer Lobby
+app.put('/lobbies/:id', async (req, res) => {
+  // Beginne Transaktion
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const lobby = await Lobby.findById(req.params.id);
+    if (!lobby || lobby.user2) {
+      throw new Error('Lobby not found or already full');
+    }
+    lobby.user2 = req.body.user;
+    await lobby.save();
+    await session.commitTransaction();
+    sendNotification(lobby);
+    res.send('Success');
+  } catch (error) {
+    await session.abortTransaction();
+    res.status(400).send(error.message);
+  }
+});
+
+// Server starten
+server.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import openSocket from 'socket.io-client';
+
+const socket = openSocket('http://localhost:3001');
 
 function App() {
+  const [user, setUser] = useState('');
+  const [lobbyId, setLobbyId] = useState('');
+  const [lobbies, setLobbies] = useState([]);
+  const [opponent, setOpponent] = useState('');
+
+  // Benachrichtigungen empfangen
+  useEffect(() => {
+    socket.on('lobby-update', (lobby) => {
+      console.log(lobby);
+      if(lobby.user2){
+        setOpponent(lobby.user2);
+        console.log('done');
+      }else {
+        console.log('not done');
+      }
+      console.log(`Lobby update: ${lobby.user1} + ${lobby.user2}`);
+    });
+  }, []);
+
+  // Funktion zum Erstellen einer Lobby
+  const createLobby = async () => {
+    if(user){    
+      const response = await axios.post('http://localhost:3001/lobbies', { user });
+      setLobbyId(response.data);
+    }else {
+      alert('Username is empty!');
+    }    
+  };
+
+  // Funktion zum Suchen verfügbarer Lobbies
+  const findLobbies = async () => {
+
+    const response = await axios.get('http://localhost:3001/lobbies');
+    setLobbies(response.data);
+  };
+
+  // Funktion zum Beitreten einer Lobby
+  const joinLobby = async (lobby) => {
+    if(user){
+      await axios.put(`http://localhost:3001/lobbies/${lobby._id}`, { user });
+      setLobbyId(lobby._id);
+      setOpponent(lobby.user1);  
+    }else {
+      alert('Username is empty!');
+    }  
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App-header'>
+        <h1>Matchmaking</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        {!lobbyId ? (
+          <>
+            <button onClick={createLobby}>Create Lobby</button>
+            <button onClick={findLobbies}>Find Lobbies</button>
+            {lobbies.length > 0 && (
+              <ul>
+                {lobbies.map((lobby) => (
+                  <div key={lobby._id}>
+                    <button onClick={() => joinLobby(lobby)}>Join {lobby.user1}</button>
+                  </div>
+                ))}
+              </ul>
+            )}
+          </>
+        ) : (
+          !opponent ? (
+              <p>Waiting for opponent in lobby {lobbyId}</p>
+            ) : (
+              <p>Matched with {opponent}</p>  
+            )
+          
+        )}
+      </div>
     </div>
   );
 }
